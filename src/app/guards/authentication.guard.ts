6b1f147f96f38eb55f8da7b7ba2fd3bf80eab8c2
theVieira <gabrielvieiraguimaraes4@gmail.com>
2025-04-11 17:07:40 -0400

    fix: verify platform to get local storage items

--- src/app/guards/authentication.guard.ts
+++ src/app/guards/authentication.guard.ts
@@ -1,9 +1,13 @@
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AuthenticationService } from '@/services/authentication.service';
+import { isPlatformBrowser } from '@angular/common';
 
 export const authenticationGuard: CanActivateFn = (route, state) => {
   const authenticationService = inject(AuthenticationService);
+  const platformId = inject(PLATFORM_ID);
+
+  if (!isPlatformBrowser(platformId)) return false;
 
   if (authenticationService.isAuthenticated()) return true;
 
