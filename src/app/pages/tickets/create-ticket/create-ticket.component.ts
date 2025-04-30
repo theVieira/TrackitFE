4ea1283f858a5433a6b6956328eb52d2b9280f88
theVieira <gabrielvieiraguimaraes4@gmail.com>
2025-04-29 20:32:40 -0400

    feat: add create ticket page

--- src/app/pages/tickets/create-ticket/create-ticket.component.ts
+++ src/app/pages/tickets/create-ticket/create-ticket.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ClientComponent } from '../../../components/filters/client/client.component';
 
 @Component({
   selector: 'app-create-new-ticket',
-  imports: [],
+  imports: [
+    MatCardModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    ClientComponent,
+  ],
   templateUrl: './create-ticket.component.html',
   styleUrl: './create-ticket.component.scss',
 })
-export class CreateTicketComponent {}
+export class CreateTicketComponent {
+  private formBuilder = inject(NonNullableFormBuilder);
+
+  protected form = this.formBuilder.group({
+    client: ['', Validators.required],
+  });
+
+  onSubmit(e: Event) {}
+
+  onClientSelected(e: string) {
+    this.form.controls.client.setValue(e);
+    console.log(this.form.controls.client.value);
+  }
+}
