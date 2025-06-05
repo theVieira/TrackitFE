d09c3ecaaac353bee1e3fbc9ce271468694ad07c
theVieira <gabrielvieiraguimaraes4@gmail.com>
2025-06-04 22:02:15 -0400

    feat: create theme switcher

--- src/app/widgets/components/theme-switcher/theme-switcher.component.ts
+++ src/app/widgets/components/theme-switcher/theme-switcher.component.ts
@@ -1,63 +1,34 @@
-import { Component, inject } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { Component, effect, inject, signal } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
-import { StorageService } from '@core/services/storage.service';
-
-interface iTheme {
-  name: string;
-  filename: string;
-}
+import { ThemeService } from '@core/services/theme.service';
+import { TranslocoModule } from '@jsverse/transloco';
+import { THEMES_CONST } from '@widgets/constants/themes.constant';
+import { iTheme } from '@widgets/interfaces/theme.interface';
 
 @Component({
   selector: 'app-theme-switcher',
-  imports: [MatFormFieldModule, MatSelectModule],
+  imports: [MatSelectModule, TranslocoModule],
   templateUrl: './theme-switcher.component.html',
 })
 export class ThemeSwitcherComponent {
-  private readonly _storageService = inject(StorageService);
-  private readonly _themeCookieName = '_theme';
+  private readonly _themeService = inject(ThemeService);
 
-  protected themes: iTheme[] = [
-    { name: 'Deep Purple & Amber', filename: 'deeppurple-amber.css' },
-    { name: 'Indigo & Pink', filename: 'indigo-pink.css' },
-    { name: 'Pink & Blue Grey', filename: 'pink-bluegrey.css' },
-    { name: 'Purple & Green', filename: 'purple-green.css' },
-    { name: 'Azure & Blue', filename: 'azure-blue.css' },
-    { name: 'Cyan & Orange', filename: 'cyan-orange.css' },
-  ];
+  private readonly themes = THEMES_CONST;
 
-  protected currentTheme = '';
+  protected darkThemes = this.themes.filter((t) => t.type === 'dark');
+  protected lightThemes = this.themes.filter((t) => t.type === 'light');
 
-  constructor() {
-    const existsThemeStorage = this._storageService.getItem<iTheme>(
-      this._themeCookieName
-    );
+  private readonly _effect = effect(() => {
+    const { filename, name, type } = this.selectedTheme();
 
-    if (existsThemeStorage) {
-      this.setTheme(existsThemeStorage);
-      return;
-    }
+    this._themeService.setTheme({ filename, name, type });
+  });
 
-    this.setTheme(this.themes[0]);
-  }
+  protected selectedTheme = signal<iTheme>(
+    this._themeService.getCurrentTheme() ?? THEMES_CONST[0]
+  );
 
-  protected setTheme({ filename, name }: iTheme): void {
-    const themeId = 'app-theme-link';
-    const existingLink = document.getElementById(themeId) as HTMLLinkElement;
-
-    const href = `assets/themes/${filename}`;
-
-    if (existingLink) {
-      existingLink.href = href;
-    } else {
-      const link = document.createElement('link');
-      link.id = themeId;
-      link.rel = 'stylesheet';
-      link.href = href;
-      document.head.appendChild(link);
-    }
-
-    this.currentTheme = name;
-    this._storageService.setItem(this._themeCookieName, filename);
+  protected onThemeChange(theme: iTheme): void {
+    this.selectedTheme.set(theme);
   }
 }
