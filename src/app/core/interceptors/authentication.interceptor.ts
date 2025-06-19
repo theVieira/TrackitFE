a39863484e410c4213b4e54130324b75310bccf4
theVieira <gabrielvieiraguimaraes4@gmail.com>
2025-06-18 22:06:10 -0400

    fix: replace use local storage to storage service

--- src/app/core/interceptors/authentication.interceptor.ts
+++ src/app/core/interceptors/authentication.interceptor.ts
@@ -2,21 +2,20 @@ import { HttpHeaders, HttpInterceptorFn } from '@angular/common/http';
 import { inject, PLATFORM_ID } from '@angular/core';
 import { environment } from '@environment';
 import { isPlatformBrowser } from '@angular/common';
+import { StorageService } from '@core/services/storage.service';
 
 export const authenticationInterceptor: HttpInterceptorFn = (req, next) => {
+  const _storageService = inject(StorageService);
   const _platformId = inject(PLATFORM_ID);
   if (!isPlatformBrowser(_platformId)) return next(req);
 
-  const token = localStorage.getItem(environment.tokenCookieName);
+  const token = _storageService.getItem<string>(environment.tokenCookieName);
 
   if (!token) {
     return next(req);
   }
 
-  const headers = new HttpHeaders().append(
-    'Authorization',
-    'Bearer ' + JSON.parse(token)
-  );
+  const headers = new HttpHeaders().append('Authorization', 'Bearer ' + token);
 
   const cloneReq = req.clone({ headers });
 
